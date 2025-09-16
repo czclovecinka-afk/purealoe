2d4622b2b881ba3dbb1ed1907f78caf062da970c
Lukas VAsicek <lukasvasicek@Lukass-MacBook-Air.local>
2025-09-16 17:36:12 +0200

    Fix: Finalni sestaveni s opravenym base URL

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,17 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          // FIX: `process.cwd()` can cause TypeScript type errors. `path.resolve('.')` is a reliable alternative that resolves to the project root.
-          '@': path.resolve('.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: '/purealoe/', // Tento řádek jsme přidali.
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve('.'),
       }
-    };
+    }
+  };
 });
